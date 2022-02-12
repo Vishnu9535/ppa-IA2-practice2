2c390382202e4e385fda12414c77b7361837922a
vishnu kumar <97020373+Vishnu9535@users.noreply.github.com>
2022-02-12 06:04:41 +0000

    p4

--- p4original.c
+++ p4original.c
@@ -13,13 +13,19 @@ void input_array(int n,int a[n]){
   }
 }
 int sum_composite_numbers(int n,int a[n]){
-  int sum=0;
+  int sum=0,flag=0;
   for(int i=0;i<n;i++){
     for(int j=2;j<=a[i]/2;j++){
       if(a[i]%j==0){
-        sum=sum+a[i];
+        flag=1;
+      }
+      else{
+        flag=0;
       }
     }
+    if(flag==1){
+      sum=sum+a[i];
+    }3
   }
   return sum;
 }
