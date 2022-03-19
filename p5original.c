e1ddf5269bcbc6393b44a78cf1762e86fca4025a
vishnu kumar <97020373+Vishnu9535@users.noreply.github.com>
2022-03-19 17:12:57 +0000

    p5

--- p5original.c
+++ p5original.c
@@ -1,1 +1,28 @@
 #include<stdio.h>
+void input(int a,int b){
+  printf("input two numbers");
+  scanf("%d %d",&a,&b);
+    
+}
+int gcd(int a,int b){
+  int x;
+ for(int i=1; i<=a && i<=b ;i++){
+    if(a%i==0 && b%i==0){
+      x=i;
+    }
+}
+  return x;
+   }
+void output(int a, int b, int x){
+  printf("gcd of %d %d  is %d ",a,b,x);
+}
+int main(){
+  int a,b,x;
+  input(a,b);
+  x=gcd(a,b);
+  output(a,b,x);
+  return 0; 
+}
+
+
+
