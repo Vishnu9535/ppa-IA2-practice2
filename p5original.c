ad396841e9f2f6124b2eb076d1b62aed893afd5b
vishnu kumar <97020373+Vishnu9535@users.noreply.github.com>
2022-03-19 02:16:03 +0000

    p6

--- p5original.c
+++ p5original.c
@@ -1,5 +1,1 @@
 #include<stdio.h>
-void input_string(char a[]){
-  printf("input a string");
-  
-}
---
+++ p6original.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+void input_string(char a[]){
+  printf("input a string");
+  fgets(a,20,stdin);
+}
+int str_reverse(char a[]){
+  char reversea[20];
+  
+  
+  
+}
