36ee4fc177829541d2cc95c878c79b238d4e44db
vishnu kumar <97020373+Vishnu9535@users.noreply.github.com>
2022-03-21 12:36:01 +0000

    p8

--- p8original.c
+++ p8original.c
@@ -8,7 +8,7 @@ int input_n(){
   scanf("%d",&n);
   return n;
 }
-Triangle input_triangle(){
+Triangle input_triangle()
   {
     Triangle z;
     printf("enter the base and height of triangles");
@@ -29,13 +29,40 @@ void input_n_triangles(int n, Triangle t[n])
 {
     for(int i=0;i<n;i++)
     {
-      t[i]->area=0.5*((t[i]->base)*(t[i]->height]);     
+      t[i].area=0.5*((t[i].base)*(t[i].height));     
     }
   }
 Triangle find_smallest_triangle(int n, Triangle t[n])
   {
-    for(int i=0;i<n;i++){
-      if(t[i]->area>)
+    Triangle smallest ;
+    smallest.area=t[0].area;
+    for(int i=1;i<n;i++){
+     if(smallest.area > t[i].area){
+       smallest.area = t[i].area;
+     }
+        
+   }
+    return smallest;
+  }
+  void output(int n, Triangle t[n], Triangle smallest)
+  {
+    printf("smallest of in given triangles %f",smallest.area);
+  }
+
+int main()
+{
+  int n;
+  Triangle z;
+  n=input_n();
+  z=input_triangle();
+  Triangle t[n];
+  input_n_triangles(n,t);
+  find_area(&t);
+  find_areas_n(n,t);
+  find_smallest_triangle(n,t);
   
-    }
+  return 0;  
+  
+  
+    
   }
